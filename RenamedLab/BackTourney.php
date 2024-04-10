e525f3ba48ea0fd67b0527a4b8d12cfa7e94800c
Scott Berry <sberry2@mail.bradley.edu>
2024-04-09 19:18:53 -0500

    login

---
+++ RenamedLab/BackTourney.php
@@ -0,0 +1,39 @@
+<?php
+
+// Grab User submitted information
+$username = $_POST["user"];
+$password = $_POST["pass"];
+
+
+
+// Create connection #servername, username, password, db 
+$conn = mysqli_connect("localhost", "root", "","tourney2");
+// Check connection
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
+//echo "Connected successfully";
+
+//mysql_select_db("tourney2",$con);
+
+// $username = stripcslashes($username);
+// $password = stripcslashes($password);
+// $username = mysqli_real_escape_string($conn, $username);
+// $password = mysqli_real_escape_string($conn, $password);
+
+
+$sql = "SELECT Username, Password FROM organizer where Username='$username' and Password= '$password'";
+$result = $conn->query($sql);
+$check =mysqli_fetch_array($result);
+
+if(isset($check)){
+	
+	echo 'success';
+}
+
+else
+{
+	echo 'failure';
+}
+
+?>
