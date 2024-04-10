7525ea797af6c69b209c4047b6594cc0ad916097
Scott Berry <sberry2@mail.bradley.edu>
2024-04-10 13:33:01 -0500

    tourneyPage

---
+++ RenamedLab/Page1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" 
+
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>User Login</title>
+</head>
+<body>
+
+	<div id="frm">
+    <form method="post" action="BackTourney.php" >
+	
+		<p>
+         <center><H1>Create a tournament!
+		 </center> 
+		 
+		   </p>
+         <p>
+		 <center>
+               <label>Tournament Name</label>
+                    <input type="text" name="Tname" id="Tname">
+				
+         
+        
+              <label>T_ID</label>
+                <input type="text" name="tid" id="tid">
+
+                 </center> 
+        </p>
+           <center>
+                <input type="submit" id="btn" value="Submit"/>
+                
+			</center>
+    </form>
+	</div>
+</body>
+</html>
